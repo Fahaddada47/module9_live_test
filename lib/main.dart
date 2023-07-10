4dd3701ed3734b199b4e2ae9c4c1c8d6af4d4c8f
Fahad Islam <fifahad007@gmail.com>
2023-07-10 23:59:34 +0600

    module 9 live test

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,77 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
-
-class Recipe {
-  final String title;
-  final String description;
-  final List<String> ingredients;
-
-  Recipe({
-    required this.title,
-    required this.description,
-    required this.ingredients,
-  });
-}
-
-
+import 'cons.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: RecipeListView(),
-  ));
-}
-class RecipeListView extends StatefulWidget {
-  @override
-  _RecipeListViewState createState() => _RecipeListViewState();
+  runApp(MyApp());
 }
 
-class _RecipeListViewState extends State<RecipeListView> {
-  List<Recipe> _recipes = [];
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RecipeListWidget(),
+    );
+  }
+}
+
+
+class RecipeService {
+  static Future<List<Recipe>> fetchRecipes() async {
+    String jsonString = await rootBundle.loadString('assets/api/recipes.json');
+    final List<dynamic> jsonList = json.decode(jsonString)['recipes'];
+    return jsonList.map((json) => Recipe.fromJson(json)).toList();
+  }
+}
+
+class RecipeListWidget extends StatefulWidget {
+  @override
+  _RecipeListWidgetState createState() => _RecipeListWidgetState();
+}
+
+class _RecipeListWidgetState extends State<RecipeListWidget> {
+  late Future<List<Recipe>> _recipeListFuture;
 
   @override
   void initState() {
     super.initState();
-    _fetchRecipes();
+    _recipeListFuture = RecipeService.fetchRecipes();
   }
 
-  Future<void> _fetchRecipes() async {
-    String jsonString = await rootBundle.loadString('assets/api/recipes.json');
-    print(jsonString);
-    var jsonData = json.decode(jsonString);
-    print(jsonData);
-    List<dynamic> recipeData = jsonData['recipes'];
-    List<Recipe> recipes = [];
-    for (var item in recipeData) {
-      Recipe recipe = Recipe(
-        title: item['title'],
-        description: item['description'],
-        ingredients: List<String>.from(item['ingredients']),
-      );
-      recipes.add(recipe);
-    }
-    setState(() {
-      _recipes = recipes;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Recipe List'),
       ),
-      body: ListView.builder(
-        itemCount: _recipes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_recipes[index].title),
-            onTap: () {
-              // Handle recipe item tap
-            },
-          );
+      body: FutureBuilder<List<Recipe>>(
+        future: _recipeListFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final recipeList = snapshot.data;
+            return ListView.builder(
+              itemCount: recipeList!.length,
+              itemBuilder: (context, index) {
+                final recipe = recipeList[index];
+                return ListTile(
+                  title: Text(recipe.title),
+                  subtitle: Text(recipe.description),
+                  onTap: () {
+                    // Handle recipe item tap
+                  },
+                );
+              },
+            );
+          }
         },
       ),
     );
